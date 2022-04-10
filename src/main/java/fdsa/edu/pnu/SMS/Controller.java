533df1c41dfe4482f4fc55523a9b0ce7ab00d7eb
Girbs <bijou.girbs@gmail.com>
2022-04-10 10:20:55 -0400

    General Update

--- src/main/java/fdsa/edu/pnu/SMS/Controller.java
+++ src/main/java/fdsa/edu/pnu/SMS/Controller.java
@@ -1,24 +1,24 @@
-package fdsa.edu.pnu.SMS;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("api/v1/sms")
-public class Controller {
-
-    private final Service service;
-
-    @Autowired
-    public Controller(Service service) {
-        this.service = service;
-    }
-
-    @PostMapping
-    public void sendSms( @RequestBody SmsRequest smsRequest) {
-        service.sendSms(smsRequest);
-    }
-}
+//package fdsa.edu.pnu.SMS;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("api/v1/sms")
+//public class Controller {
+//
+//    private final Service service;
+//
+//    @Autowired
+//    public Controller(Service service) {
+//        this.service = service;
+//    }
+//
+//    @PostMapping
+//    public void sendSms( @RequestBody SmsRequest smsRequest) {
+//        service.sendSms(smsRequest);
+//    }
+//}
