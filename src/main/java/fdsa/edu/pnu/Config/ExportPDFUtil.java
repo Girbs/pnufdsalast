23e2298fbe14bcd0719597bb4ee2bc4722353ad8
girbs <bijou.girbs@gmail.com>
2022-11-06 14:43:50 -0500

    PNU-5 Correction upload Fichier

--- src/main/java/fdsa/edu/pnu/Config/ExportPDFUtil.java
+++ src/main/java/fdsa/edu/pnu/Config/ExportPDFUtil.java
@@ -9,9 +9,7 @@ import fdsa.edu.pnu.Model.Postulant;
 import fdsa.edu.pnu.Repository.PostulantDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.io.IOException;
 
 @Service
@@ -22,11 +20,11 @@ public class ExportPDFUtil {
 
     private void writeTableHeader(PdfPTable table) {
         PdfPCell cell = new PdfPCell();
-        cell.setBackgroundColor(Color.BLUE);
+        // cell.setBackgroundColor(Color.BLUE);
         cell.setPadding(5);
 
         Font font = FontFactory.getFont(FontFactory.HELVETICA);
-        font.setColor(Color.BLACK);
+        // font.setColor(Color.BLACK);
 
         cell.setPhrase(new Phrase("Id", font));
         table.addCell(cell);
@@ -49,13 +47,16 @@ public class ExportPDFUtil {
 
     private void writeTableData(PdfPTable table, Postulant postulant) throws IOException {
 //        for (Postulant postulant : studentList) {
-            //Image image = Image.getInstance(postulant.getPhotoProfil());
-           // table.addCell(String.valueOf(postulant.getId()));
-           // table.addCell(image);
-            table.addCell(postulant.getNom());
-            table.addCell(postulant.getPrenom());
-            table.addCell(postulant.getEmail());
-            //table.addCell(postulant.getLienFichier());
+        //Image image = Image.getInstance(postulant.getPhotoProfil());
+        // table.addCell(String.valueOf(postulant.getId()));
+        // table.addCell(image);
+        table.addCell(postulant.getNom());
+        table.addCell(postulant.getPrenom());
+        table.addCell(postulant.getEmail());
+        table.addCell(postulant.getNom());
+        table.addCell(postulant.getPrenom());
+        table.addCell(postulant.getEmail());
+        //table.addCell(postulant.getLienFichier());
 //            break;
 //        }
     }
@@ -63,31 +64,54 @@ public class ExportPDFUtil {
     public void export(HttpServletResponse response, Integer id) throws DocumentException, IOException {
         Document document = new Document(PageSize.A4);
         PdfWriter.getInstance(document, response.getOutputStream());
-
         document.open();
+
         Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
         font.setSize(18);
-        font.setColor(Color.ORANGE);
-
-        Paragraph p = new Paragraph("Here's the exported data", font);
+        //font.setColor(Color.BLUE);
+        Postulant postulant = postulantDAO.findById(id).get();
+        Paragraph p = new Paragraph("Application de "+postulant.getNom() +" "+postulant.getPrenom(), font);
+        Paragraph p1 = new Paragraph("Numero de dossier: "+postulant.getMatricule());
         p.setAlignment(Paragraph.ALIGN_CENTER);
-
         document.add(p);
+        document.add(p1);
 
-        PdfPTable table = new PdfPTable(6);
-        table.setWidthPercentage(100f);
-        table.setWidths(new float[]{0.5f, 1.0f, 3.5f, 3.0f, 3.0f, 1.5f});
-        table.setSpacingBefore(10);
+        float col1 = 800f;
+        // public static final Border NO_BORDER = null;
+//        int col2 = col1+150;
+//        float mainInfo[]={col1, col2};
+        float columnWidth [] = {col1, col1};
+        Table t = new Table(2);
+        t.setBorder(0);
+        t.addCell(postulant.getMatricule());
+        t.addCell(postulant.getPrenom());
+        // subTitle.addCell(postulate.getNom());
+        document.add(t);
 
-        writeTableHeader(table);
 
-       Postulant postulant = postulantDAO.findById(id).get();
-      //  writeTableData(table, postulant);
+
+        //Add content to the document using Phrase objects.
+        document.add(new Phrase("Test Phrase1."));
+        document.add(new Phrase("Test Phrase2."));
+
+
+
+//        PdfPTable table = new PdfPTable();
+//        table.setWidthPercentage(100f);
+//        table.setWidths(new float[]{0.5f, 1.0f, 3.5f, 3.0f, 3.0f, 1.5f});
+//        table.setSpacingBefore(10);
+//
+//        writeTableHeader(table);
+
+        // Postulant postulant = postulantDAO.findById(id).get();
+        //  writeTableData(table, postulant);
 //        List<Postulant> postulants = postulantDAO.findAll();
 //        writeTableData(table, postulants);
-        table.addCell("TEXT");
+//        table.addCell("TEXT");
+//        table.addCell("TEXT");
+//        table.addCell("TEXT");
 
-        document.add(table);
+        // document.add(table);
 
         document.close();
 
